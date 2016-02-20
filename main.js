3625340d380b2daaa001cf74900c8fd369d4b879
paulkass <pkassian@gmail.com>
2016-02-20 11:53:43 -0800

    Some changes to port listening

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ console.log("app started")
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+
+});
