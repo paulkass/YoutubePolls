3e08a6d5c0cb85c82568ef6902d28678ae6356f0
clarencechen <clarencechenct@gmail.com>
2016-02-20 18:52:55 -0800

    Changed "login box" to search box

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-function loadBoard() {
-	//replace with server technology
-	var fields = JSON.parse(localStorage.getItem('results'));
-	//end replace
-	var tableappend = "";
-	for(var j = 0; j < fields.length; j++)
-		tableappend += "<tr><td>" + fields[j].name + "</td><td>" + fields[j].rating + "</td><td>" + "" + "</td></tr>";
-	$("tbody#results").append(tableappend);
-}
+$(document).ready(load);
 
 function load() {
 	$("section#intro").append("<p>Welcome to YoutubePolls!</p>");
 }
-$(document).ready(function() {
-	load();
-});
-$("tbody#results").ready(loadBoard);
+
+
+$("section#pollresults").ready(loadBoard);
+
+function loadBoard() {
+	var fields = JSON.parse(localStorage.getItem('results'));
+	if(fields === null)
+	{
+		$("section#resultsHead").append("<p>No Results</p>");
+	}
+	else
+	{
+		$("section#resultsHead").append("<p>" + fields.length + " Results</p>");
+		$("section#pollresults").append(
+			'<table class="col-sm-12"><thead><tr><th>Video Name</th><th>Video Rating</th><th>Comments</th></tr></thead><tbody id="results"></tbody></table>'
+		);
+		var tableappend = "";
+		for(var j = 0; j < fields.length; j++)
+			tableappend += "<tr><td>" + fields[j].name + "</td><td>" + fields[j].rating + "</td><td>" + "" + "</td></tr>";
+		$("tbody#results").append(tableappend);
+	}
+}
