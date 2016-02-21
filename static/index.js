771e600d2e412c5df89beb270e5bcbacbac6f7d6
paulkass <pkassian@gmail.com>
2016-02-20 23:11:01 -0800

    removed annoying alerts

--- static/index.js
+++ static/index.js
@@ -7,11 +7,12 @@ $(document).ready(function() {
 // 	};
 
 });
-		var host = location.origin.replace(/^http/, 'ws')
-      var ws = new WebSocket(host);
-      ws.onmessage = function (event) {
-        alert(JSON.parse(event.data));
-		}
+
+var host = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(host);
+ws.onmessage = function (event) {
+    
+}
 	
 
 function load() {
