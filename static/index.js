eaadf3552fdfef08c390e029a3bba8c40586f1be
clarencechen <clarencechenct@gmail.com>
2016-02-21 09:10:37 -0800

    reinstate };

--- static/index.js
+++ static/index.js
@@ -9,34 +9,58 @@ $(document).ready(function() {
 			submitQuery();
 	});
  	ws.onmessage = function(event){
-	//loadBoard(event.data);
-	var data = event.data
-	var stub = data.split("::")[0]
-	var obj = JSON.parse(data.split("::")[1])
-	$("section#resultsHead").html('')
-	$("#chart").css({
-		height: window.innerHeight*0.3,
-		width: window.innerWidth*0.5
-	})
-	};
-	var ctx = document.getElementById("chart").getContext("2d");
-	var chartData = [
-		{
-			value: obj.positive_count,
-			color: "#46BFBD",
-			highlight: "#5AD3D1",
-			label: "Positive"
-		},
-		{
-			value: obj.negative_count,
-			color: "#F7464A",
-			highlight: "#FF5A5E",
-			label: "Negative"
+		var data = event.data
+		var stub = data.split("::")[0]
+		var obj = JSON.parse(data.split("::")[1])
+		$("section#resultsHead").html('')
+		$("#chart").css({
+			height: window.innerHeight*0.5,
+			width: window.innerWidth
+		})
+		var ctx = document.getElementById("chart").getContext("2d");
+		var chartData = [
+			{
+				value: obj.positive_count,
+				color: "#46BFBD",
+				highlight: "#5AD3D1",
+				label: "Positive"
+			},
+			{
+				value: obj.negative_count,
+				color: "#F7464A",
+				highlight: "#FF5A5E",
+				label: "Negative"
+			}
+		]
+		var chartOptions = {
+   			 //Boolean - Whether we should show a stroke on each segment
+    		segmentShowStroke : true,
+
+    		//String - The colour of each segment stroke
+    		segmentStrokeColor : "#fff",
+
+    		//Number - The width of each segment stroke
+    		segmentStrokeWidth : 2,
+
+   		 	//Number - The percentage of the chart that we cut out of the middle
+    		percentageInnerCutout : 50, // This is 0 for Pie charts
+
+    		//Number - Amount of animation steps
+    		animationSteps : 100,
+
+    		//String - Animation easing effect
+    		animationEasing : "easeOutBounce",
+
+    		//Boolean - Whether we animate the rotation of the Doughnut
+    		animateRotate : true,
+
+    		//Boolean - Whether we animate scaling the Doughnut from the centre
+    		animateScale : false
 		}
-	]
-	var chartOptions = {}
-	var chart = new Chart(ctx).Doughnut(chartData, chartOptions)
-};
+
+		var chart = new Chart(ctx).Doughnut(chartData, chartOptions)
+	};
+});
 
 
 function load() {
